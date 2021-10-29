0cb51738b759a46491960b6a4e6dd5cc645293c0
Thobela-CS <thobelacs@gmail.com>
2021-10-29 08:08:11 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and see flames.
         </p>
+        <b>But why I need to do this???</b>
         <a
           className="App-link"
           href="https://reactjs.org"
